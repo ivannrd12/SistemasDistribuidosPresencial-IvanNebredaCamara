25ef91b7620415d98f3cd0fdce797eb73cd8e2fe
Ivan Nebreda <ivannebre03@gmail.com>
2025-03-14 18:10:28 +0100

    Documentacion interfaz

--- ChatSockets/src/main/java/es/ubu/lsi/server/ChatServer.java
+++ ChatSockets/src/main/java/es/ubu/lsi/server/ChatServer.java
@@ -6,12 +6,26 @@ import es.ubu.lsi.common.ChatMessage;
  * Interfaz que define los métodos principales para un servidor de chat.
  */	
 public interface ChatServer {
-
+	
+	/**
+     * Inicia el servidor y comienza a aceptar conexiones entrantes.
+     */
 	void startup();
 	
+	/**
+     * Apaga el servidor, cerrando todos los sockets abiertos y liberando recursos.
+     */
 	void shutdown();
 	
+	/**
+     * Difunde un mensaje de chat a todos los clientes conectados.
+     * @param message Objeto ChatMessage que contiene los datos del mensaje a enviar.
+     */
 	void broadcast(ChatMessage message);
 	
+	/**
+     * Elimina (desconecta) al cliente con el identificador especificado.
+     * @param id Identificador único del cliente que se va a remover.
+     */
 	void remove(int id);
 }
