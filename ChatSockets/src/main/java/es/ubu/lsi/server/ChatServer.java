2552cfac5b10e4f86310ac286d4ad0641683751e
Ivan Nebreda <ivannebre03@gmail.com>
2025-03-14 18:08:19 +0100

    interfaz y atributos del servidor

--- ChatSockets/src/main/java/es/ubu/lsi/server/ChatServer.java
+++ ChatSockets/src/main/java/es/ubu/lsi/server/ChatServer.java
@@ -1,5 +1,17 @@
 package es.ubu.lsi.server;
 
+import es.ubu.lsi.common.ChatMessage;
+
+/**
+ * Interfaz que define los métodos principales para un servidor de chat.
+ */	
 public interface ChatServer {
 
+	void startup();
+	
+	void shutdown();
+	
+	void broadcast(ChatMessage message);
+	
+	void remove(int id);
 }
--- ChatSockets/src/main/java/es/ubu/lsi/server/ChatServerImpl.java
+++ ChatSockets/src/main/java/es/ubu/lsi/server/ChatServerImpl.java
@@ -1,5 +1,40 @@
 package es.ubu.lsi.server;
 
-public class ChatServerImpl {
+import java.text.SimpleDateFormat;
+
+import es.ubu.lsi.common.ChatMessage;
+
+public class ChatServerImpl implements ChatServer {
+
+	private static final int DEFAULT_PORT = 1500;
+	private static int clientId;
+	private static SimpleDateFormat sdf;
+	private int port;
+	private boolean alive;
+	
+	
+	@Override
+	public void startup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void broadcast(ChatMessage message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(int id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
