b3be6609547e8ebfa1fbcb3928001482d030a294
Ivan Nebreda <ivannebre03@gmail.com>
2025-04-30 13:40:31 +0200

    spring boot con dependencias inicales para la practica 3

---
+++ Practica3/src/test/java/com/p03/Practica3/Practica3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p03.Practica3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
